cb4fc259d8e1a7c6f488239f32f7986e22941c0a
aryajava <muhammad.aryajava@gmail.com>
2024-10-10 20:58:37 +0700

    Fix session, ensure session data is passed to route

--- routes/todo.js
+++ routes/todo.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { getAll, getById, create, update, remove } = require("../models/Todo.js");
 
-router.get("/", function (req, res, next) {
+function checkSession(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+router.get("/", checkSession, function (req, res, next) {
   getAll().then((todos) => {
     res.render("todos", {
       title: "BREADS (Browse, Read, Edit, Add, Delete, Sort)",
@@ -12,31 +19,31 @@ router.get("/", function (req, res, next) {
   });
 });
 
-router.get("/add", function (req, res, next) {
+router.get("/add", checkSession, function (req, res, next) {
   res.render("todos/form", { title: "Add Todo" });
 });
 
-router.post("/add", function (req, res, next) {
+router.post("/add", checkSession, function (req, res, next) {
   const { title, description } = req.body;
   create(title, description).then(() => {
     res.redirect("todos");
   });
 });
 
-router.get("/edit/:id", function (req, res, next) {
+router.get("/edit/:id", checkSession, function (req, res, next) {
   getById(req.params.id).then((todo) => {
     res.render("todos/form", { title: "Edit Todo", todo });
   });
 });
 
-router.post("/edit/:id", function (req, res, next) {
+router.post("/edit/:id", checkSession, function (req, res, next) {
   const { title, description } = req.body;
   update(req.params.id, title, description).then(() => {
     res.redirect("todos");
   });
 });
 
-router.get("/delete/:id", function (req, res, next) {
+router.get("/delete/:id", checkSession, function (req, res, next) {
   remove(req.params.id).then(() => {
     res.redirect("todos");
   });
